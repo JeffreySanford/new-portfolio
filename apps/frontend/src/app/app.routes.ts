e2f0cc61e68aa48c82597921238be1a7e363b893
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-08-10 07:18:58 -0500

    chore: Remove unused routes and modules

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -2,14 +2,6 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 
 export const appRoutes: Routes = [
-  {
-    path: 'auth',
-    loadChildren: () => import('./common/auth/auth.module').then(m => m.AuthModule),
-  },
-  {
-    path: 'users',
-    loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
-  },
   { path: 'home', component: LandingComponent },
   { path: 'product', component: LandingComponent },
   { path: 'contact', component: LandingComponent },
