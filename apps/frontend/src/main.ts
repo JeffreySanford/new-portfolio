ae576d2578772f27fed4de2c6990d9f20575001f
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-08-27 10:53:27 -0500

    chore: Update project dependencies and enable CORS

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,8 +1,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { from } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule, {
+const bootstrap$ = from(
+  platformBrowserDynamic().bootstrapModule(AppModule, {
     ngZoneEventCoalescing: true,
   })
-  .catch((err) => console.error(err));
+).pipe(
+  catchError((err) => {
+    console.error(err);
+    return [];
+  })
+);
+
+bootstrap$.subscribe();
