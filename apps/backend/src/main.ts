7d68c340f53db0b84e619e7aaeb8a404a134bbae
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-08-30 20:39:13 -0500

    chore: Adjust CORS origin in main.ts and proxy.conf.json
    Update the CORS origin in the main.ts file to 'http://localhost:4200' and in the proxy.conf.json file to 'http://0.0.0.0:4200'. This ensures that the backend server allows requests from the frontend URL and correctly forwards requests to the '/api' endpoint.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://0.0.0.0:4200'],
+    origin: 'http://localhost:4200', // Adjust this to your frontend's URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
