005fe5be149ff62d0a9ea14d907225496a9d0ee1
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-08-30 19:36:39 -0500

    chore: Update main.ts with portfolio information
    Update the title and description in the main.ts file to reflect the portfolio of Jeffrey Sanford. Also, update the tag from 'cats' to 'portfolio' to better represent the content of the API. This commit improves the accuracy and relevance of the portfolio information in the application.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,16 +7,16 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:4200', // Adjust this to your frontend's URL
+    origin: 'http://0.0.0.0:4200', // Adjust this to your frontend's URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
   const options = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Jeffrey Sanford')
+    .setDescription('Portfolio for Jeffrey Sanford')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('portfolio')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
