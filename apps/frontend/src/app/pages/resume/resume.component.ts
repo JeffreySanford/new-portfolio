92a1b1306a4f2360e63da861169aab841e04632d
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-09-06 10:35:59 -0500

    Refactor ResumeComponent to remove unused code and dependencies

--- apps/frontend/src/app/pages/resume/resume.component.ts
+++ apps/frontend/src/app/pages/resume/resume.component.ts
@@ -1,25 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls:  ['./resume.component.scss']
 })
-export class ResumeComponent implements OnInit {
-  pdfSrc = '../../../assets/documents/Resume - Jeffrey Sanford - Web Developer.pdf';
-  
-
-  constructor() { }
-
-  ngAfterViewInit() {
-    debugger
-    this.pdfSrc = '../../../assets/documents/Resume - Jeffrey Sanford - Web Developer.pdf';
-    console.log('Source:', this.pdfSrc);
-  }
-  ngOnInit(): void {
-    debugger
-    this.pdfSrc = '../../../assets/documents/Resume - Jeffrey Sanford - Web Developer.pdf';
-    console.log('Source:', this.pdfSrc);
-
-  }
+export class ResumeComponent {
 }
