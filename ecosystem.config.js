3ab52b895b3b5ff1b7cdf00fe4d7c5372709e022
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-11-14 13:53:56 -0600

    fix: update output paths in project configuration and ecosystem script for backend

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'Backend API',
-      script: 'dist/apps/backend/main.js',
+      script: 'dist/main.js',
       instances: 1,
       autorestart: true,
       max_memory_restart: '1G',
